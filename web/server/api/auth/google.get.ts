be83acfc1ef9f9f7f1e6c5bad019a44939340fc8
Bagas Yadher Bima Nugraha Ainur Rofiq Hidayat <bagas.ybnarh@gmail.com>
2025-02-06 17:20:48 +0700

    try fix

--- web/server/api/auth/google.get.ts
+++ web/server/api/auth/google.get.ts
@@ -1,16 +1,16 @@
 import { useState } from "nuxt/app";
 
-// export default defineOAuthGoogleEventHandler({
-//   async onError(event, error) {
-//     return console.log("error happened", error, event);
-//   },
-//   async onSuccess(event, { user }) {
-//     user.admin = user.email === process.env.ADMIN_EMAIL && user.sub === process.env.ADMIN_SUB;
-//     await setUserSession(event, { user });
-//     return sendRedirect(event, "/");
-//   },
-// });
-export default defineEventHandler((event) => {
-  console.log(event)
-  return "Auth can be accessed";
-})
+export default defineOAuthGoogleEventHandler({
+  async onError(event, error) {
+    return console.log("error happened", error, event);
+  },
+  async onSuccess(event, { user }) {
+    user.admin = user.email === process.env.ADMIN_EMAIL && user.sub === process.env.ADMIN_SUB;
+    await setUserSession(event, { user });
+    return sendRedirect(event, "/");
+  },
+});
+// export default defineEventHandler((event) => {
+//   console.log(event)
+//   return "Auth can be accessed";
+// })
