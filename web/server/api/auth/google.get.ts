f199155fa4a325eb184de85a1e9824c5ea34828a
Bagas Yadher Bima Nugraha Ainur Rofiq Hidayat <bagas.ybnarh@gmail.com>
2025-02-06 17:02:45 +0700

    Added debug for auth google

--- web/server/api/auth/google.get.ts
+++ web/server/api/auth/google.get.ts
@@ -1,6 +1,9 @@
 import { useState } from "nuxt/app";
 
 export default defineOAuthGoogleEventHandler({
+  async onError(event, error) {
+    return console.log("error happened", error, event);
+  },
   async onSuccess(event, { user }) {
     user.admin = user.email === process.env.ADMIN_EMAIL && user.sub === process.env.ADMIN_SUB;
     await setUserSession(event, { user });
