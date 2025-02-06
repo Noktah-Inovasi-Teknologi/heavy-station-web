5ce48122b53d5bbd877934992ad3156ec29ac35f
Bagas Yadher Bima Nugraha Ainur Rofiq Hidayat <bagas.ybnarh@gmail.com>
2025-02-06 18:22:35 +0700

    fix3

--- web/server/api/auth/google.get.ts
+++ web/server/api/auth/google.get.ts
@@ -1,10 +1,14 @@
 import { useState } from "nuxt/app";
 
 export default defineOAuthGoogleEventHandler({
-  async onError(event, error) {
-    return console.log("error happened", error, event);
-  },
+  // config(event) {
+    
+  // }
+  // async onError(event, error) {
+  //   return console.log("error happened", error, event);
+  // },
   async onSuccess(event, { user }) {
+    console.log("This actually succeed", event, user)
     user.admin = user.email === process.env.ADMIN_EMAIL && user.sub === process.env.ADMIN_SUB;
     await setUserSession(event, { user });
     return sendRedirect(event, "/");
