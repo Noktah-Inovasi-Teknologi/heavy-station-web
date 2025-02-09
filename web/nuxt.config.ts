44284e0ac060e2452141f776540613cfa2ab131e
Bagas Yadher Bima Nugraha Ainur Rofiq Hidayat <bagas.ybnarh@gmail.com>
2025-02-09 13:53:52 +0700

    added env to public

--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -77,5 +77,14 @@ export default defineNuxtConfig({
         redirectURL: process.env.GOOGLE_REDIRECT_URL,
       },
     },
+    public: {
+      authDomain: process.env.NUXT_KINDE_AUTH_DOMAIN,
+      clientId: process.env.NUXT_KINDE_CLIENT_ID,
+      clientSecret: process.env.NUXT_KINDE_CLIENT_SECRET,
+      redirectURL: process.env.NUXT_KINDE_REDIRECT_URL,
+      logoutRedirectURL: process.env.NUXT_KINDE_LOGOUT_REDIRECT_URL,
+      postLoginRedirectURL: process.env.NUXT_KINDE_POST_LOGIN_REDIRECT_URL,
+      password: process.env.NUXT_KINDE_PASSWORD,
+    }
   },
 });
