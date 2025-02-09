559b1ed42991a2a83707bda427c21232af68eb03
Bagas Yadher Bima Nugraha Ainur Rofiq Hidayat <bagas.ybnarh@gmail.com>
2025-02-09 08:00:44 +0700

    Make debug true

--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -24,6 +24,7 @@ export default defineNuxtConfig({
   },
   compatibilityDate: "2024-11-01",
   css: ["@/assets/styles/tailwind.css", "@/assets/styles/base.css"],
+  debug: true,
   devtools: { enabled: true },
   nitro: {
     experimental: {
